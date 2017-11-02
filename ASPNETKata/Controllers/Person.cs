30a2c6e2500ebcab3bf0f832838623286efd46f5
KaDarius King <kdar09@gmail.com>
2017-11-02 13:12:02 -0500

    added people for list

---
+++ ASPNETKata/Controllers/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETKata.Controllers
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool IsOwnerMinor { get; set; }
+    }
+}
